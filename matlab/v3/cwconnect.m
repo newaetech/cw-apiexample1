e56007e68237f6aaf97812b4f2fac2c679dc8446
FranzHeubach <franz.heubach@gmail.com>
2018-03-01 13:56:27 -0400

    Move all previous file into v3 subfolder
    This way if we need the v3 version of any of the files we have them.

---
+++ matlab/v3/cwconnect.m
@@ -0,0 +1,19 @@
+function [ cwapi ] = cwconnect( offset, totalsamples )
+%CWCONNECT Connect to a ChipWhisperer-Lite, return the object.
+%   Connects to a ChipWhisperer-Lite. You must have already configured
+%   python for this to work & have a WORKING ChipWhisperer install.s
+
+if nargin < 2
+  totalsamples = 3000;
+end
+if nargin < 1
+  offset = 1250;
+end
+
+import py.cwapi.cwconnect
+
+result = cwconnect(offset, totalsamples);
+
+
+end
+
